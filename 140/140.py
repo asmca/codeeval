6fc5c14060e7c48c4752be87df5265bdb42d504a
Suse Shi <zip@asmca.net>
2014-05-12 19:02:44 +0800

    problem 140 in progress

---
+++ 140/140.py
@@ -0,0 +1,23 @@
+import sys
+
+
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    test1,test2=test.strip().split(';')
+    s1=test1.split()
+    c=[int(x) for x in test2.split()]
+
+    s2=s1
+    for i,n in enumerate(c):
+        s2[n-1]=s1[i]
+    
+    print s2
+
+
+    
+
+
+
+test_cases.close()
+
