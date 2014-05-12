a98800c6f6191da7924ed0462966f5de3033ad3a
asmca <zip@asmca.net>
2014-05-12 22:45:59 +0800

    problem 140 solved.

--- 140/140.py
+++ 140/140.py
@@ -1,23 +1,23 @@
 import sys
 
-# add a comment
 
 test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
     test1,test2=test.strip().split(';')
     s1=test1.split()
-    c=[int(x) for x in test2.split()]
 
-    s2=s1
+    #by default last value is not list here.
+    # append last value to list c
+    c=[int(x) for x in test2.split()]
+    for i in range(1,len(s1)+1):
+        if i not in c:
+            c.append(i)
+
+    s2=s1[:]
     for i,n in enumerate(c):
         s2[n-1]=s1[i]
     
-    print s2
-
-
-    
-
-
+    print ' '.join(s2)
 
 test_cases.close()
 
